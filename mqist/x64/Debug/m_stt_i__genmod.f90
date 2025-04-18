d8b64e5b2f0ce36b015b80db216254d3c4f5c024
David Cunningham <david.cunningham@utexas.edu>
2025-04-18 09:43:21 -0700

    clean and adjust for new build requests

---
+++ mqist/x64/Debug/m_stt_i__genmod.f90
@@ -0,0 +1,11 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Tue Apr  8 12:22:25 2025
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE M_STT_I__genmod
+          INTERFACE 
+            SUBROUTINE M_STT_I(TAU,STT_I_O)
+              REAL(KIND=8), INTENT(IN) :: TAU
+              REAL(KIND=8), INTENT(OUT) :: STT_I_O(8,8,8)
+            END SUBROUTINE M_STT_I
+          END INTERFACE 
+        END MODULE M_STT_I__genmod
