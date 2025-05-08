b8f12b7840e60c019188ab0fdb94bd8d25b01772
David Cunningham <david.cunningham@utexas.edu>
2025-05-08 15:14:17 -0700

    Caps in header

--- fort/lib/mqist.h
+++ fort/lib/mqist.h
@@ -5,18 +5,18 @@
 // david.cunningham@utexas.edu
 // Last revision: see git log
 
-void m_init_n(char* namefile_c);
+void M_INIT_N(char* namefile_c);
 
-void m_state(double* tau, double* state_o);
+void M_STATE(double* tau, double* state_o);
 
-void m_stm(double* tau, double* stm_o);
+void M_STM(double* tau, double* stm_o);
 
-void m_stt(double* tau, double* stt_o);
+void M_STT(double* tau, double* stt_o);
 
-void m_stm_i(double* tau, double* stm_i_o);
+void M_STM_I(double* tau, double* stm_i_o);
 
-void m_stt_i(double* tau, double* stt_i_o);
+void M_STT_I(double* tau, double* stt_i_o);
 
-void m_prop_once(double* ta, double* tb, double* xa, int* order, double* xb);
+void M_PROP_ONCE(double* ta, double* tb, double* xa, int* order, double* xb);
 
 void m_stts_ab(double* taua, double* taub, double* stm_o, double* stt_o);
